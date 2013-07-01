7494fd54028f1ed3479790d2046826d5d00b3cbd
David <david@Bigfoot.(none)>
2013-06-30 18:30:40 -0700

    working on die class

--- players.py
+++ players.py
@@ -4,11 +4,12 @@ class Player(object):
     def __init__(self, name, brain_count = 0):
         self.name = name
         self.brain_count = brain_count
-    def __repl__(self):
-        print self.name + " has " + str(self.brain_count) + " brains."
+    def __repr__(self):
+        return "player " + self.name + " has " + str(self.brain_count) + " brains."
     def has_won(self):
         if self.brain_count >= self.WINNING_BRAIN_COUNT:
             return True
         else:
             return False
+
 
