004ea4363c9573095c1538ed88255ff4de76bd09
David <david@Bigfoot.(none)>
2013-06-30 17:43:31 -0700

    Player class

---
+++ players.py
@@ -0,0 +1,14 @@
+class Player(object):
+    """ defines an individual player"""
+    WINNING_BRAIN_COUNT = 13
+    def __init__(self, name, brain_count = 0):
+        self.name = name
+        self.brain_count = brain_count
+    def __repl__(self):
+        print self.name + " has " + str(self.brain_count) + " brains."
+    def has_won(self):
+        if self.brain_count >= self.WINNING_BRAIN_COUNT:
+            return True
+        else:
+            return False
+
